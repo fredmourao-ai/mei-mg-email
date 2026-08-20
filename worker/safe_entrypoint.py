4c8d445e3dc29ad69155a3ed60cb953f731eacf2
Frederico mourao <fredmourao@gmail.com>
2026-08-20 12:04:22 -0300

    fix: accept explicit user authorization origin in presend guard

--- worker/safe_entrypoint.py
+++ worker/safe_entrypoint.py
@@ -29,7 +29,6 @@ LEGACY_MARKETING_ORIGINS = {
     "confirmacao_operador_2026-08-13",
     "politica_importacao_operador_2026-08-13",
     "operator_authorization_true_2026-08-20",
-    "user_explicit_authorization_2026-08-20",
 }
 LEGACY_MEI_ORIGINS = {
     "override_operador_2026-08-13",
